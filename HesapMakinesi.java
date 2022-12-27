031d3aef40dbc83934c90cd36e6362826101e268
Furkan ALAKUŞ <furkanalakus91@gmail.com>
2022-12-28 01:00:11 +0300

    metot v0001

---
+++ HesapMakinesi.java
@@ -0,0 +1,21 @@
+package metot;
+    import java.util.Scanner;
+public class recursiveHesapMakinesi {
+
+    static void sum(int a, int b) {
+
+    }
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        String menu = "1-toplama\n"
+                    +"2-çıkarma\n"
+                    +"3-çarpma\n"
+                    +"4-üslü sayı\n"
+                    +"5-Mod\n"
+                    +"6-Bölme\n"
+                    +"7-Dikdörtgen Alan\n"
+                    +"8-Çıkış yap";
+
+    }
+}
---
+++ recursiveFibonacci.java
@@ -0,0 +1,2 @@
+package metot;public class recursiveFibonacci {
+}
